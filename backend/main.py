1c55ffb0a4a8f34ea61484374ec2374c4d0f364b
harsh-tr3010 <134399264+harsh-tr3010@users.noreply.github.com>
2026-04-29 15:55:06 +0000

    changed origin

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "https://hr-chatbot-for-internal-hiring-support-frontend-jn9dlvr4q.vercel.app/",
+    "https://hr-chatbot-for-internal-hiring-support-frontend-41462lbss.vercel.app",
 ]
 
 app.add_middleware(
